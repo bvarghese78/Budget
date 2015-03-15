521fc3f2d1beaa012faf9269203d56ee7ed8a97c
Benji Varghese <benjivarghese@gmail.com>
2015-03-15 18:56:20 -0500

    Acutal import of BJ.Common

---
+++ BJ.Common/BJ.Common/TestMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BJ.Common
+{
+    [TestClass]
+    public class TestMethod
+    {
+        [TestMethod]
+        public void test()
+        {
+            DateTimeMethods time = new DateTimeMethods();
+            //var testValue = time.fullDateTime(2014, 12, 20, 13, 50, 12);
+            var testValue = time.currentMonth();
+            Assert.IsNotNull(testValue);
+        }
+    }
+}
